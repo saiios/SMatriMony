8fb08f11ca87193828d31d8f169956426f816849
sujith1545 <sujith@indobytes.com>
2017-06-07 15:29:06 +0530

    no message

--- Religious_info_editCell.m
+++ Religious_info_editCell.m
@@ -8,15 +8,17 @@
 
 #import "Religious_info_editCell.h"
 
+
 @implementation Religious_info_editCell
 
 - (void)awakeFromNib {
     [super awakeFromNib];
     // Initialization code
-}
+   }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
+    
 
     // Configure the view for the selected state
 }
